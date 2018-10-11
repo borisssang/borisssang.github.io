e73187bec09a5a8f3b7086ebf46ecdce180e3f3c
borisssang <borisssang@gmail.com>
2018-10-11 19:07:12 +0300

    functionality for adding toDo's

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
+function addTodo(text) {
+    var todoList = document.getElementById('todo-list');
+    var todo = createTodoElement(text);
+    todoList.appendChild(todo);
+}
 
+function createTodoElement(title) {
+    // Create todo li container
+    var todoLi = document.createElement('li');
+    var titleSpan = document.createElement('span');
+    titleSpan.innerText = title;
+    todoLi.appendChild(titleSpan);
+
+    // Create swipe controls
+    var controls = document.createElement('div');
+    controls.classList.add('controls');
+    var deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerText = "DELETE";
+    controls.appendChild(deleteBtn);
+
+    todoLi.appendChild(controls);
+    return todoLi;
+}
