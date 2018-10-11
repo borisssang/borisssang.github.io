5e90c87cc7c92199c23cc4b3acf02a931e32123e
borisssang <borisssang@gmail.com>
2018-10-11 16:12:21 +0300

    animaton completed

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 (() => {
+    var currentControls;
     var mouseDown = false;
     var startX = 0;
     var currentX = 0;
 
     var todos = document.getElementsByClassName('todos')[0];
     todos.addEventListener('mousedown', (event) => {
-        mouseDown = true;
         startX = event.clientX;
+        currentControls = getControls(event);
+        currentControls.classList.remove('transition');
     });
 
     todos.addEventListener('mouseup', (event) => {
-        mouseDown = false;
-        var controls = getControls(event);
-        //controls.style.marginRight = -controls.offsetWidth;
+        resetControls();
+        currentControls = null;
     });
 
     todos.addEventListener('mouseout', (event) => {
-        mouseDown = false;
+        resetControls();
+        currentControls = null;
     });
 
     todos.addEventListener('mousemove', (event) => {
-        if (!mouseDown) return;
+        if (!currentControls) return;
 
         currentX = event.clientX;
         var diff = startX - currentX;
-        var controls = getControls(event);
-        if (diff > controls.offsetWidth / 2) {
-            controls.style.removeProperty('margin-right');
-            controls.classList.add('visible');
-        } 
-        else if (diff > 0 && !controls.classList.contains('visible')) {   
-            var width = controls.offsetWidth;
+
+        if (diff > 0 && !currentControls.classList.contains('visible')) {   
+            var width = currentControls.offsetWidth;
             var margin = diff > width ? 0 : -width + diff;
-            controls.style.marginRight = margin + 'px';
-        }
-        else if (diff < -controls.offsetWidth / 2) {
-            controls.style.removeProperty('margin-right');
-            controls.classList.remove('visible');
+            currentControls.style.marginRight = margin + 'px';
         }
         else if (diff < 0) {
-            var width = controls.offsetWidth;
+            var width = currentControls.offsetWidth;
             var margin = diff < -width ? -width : diff;
-            controls.style.marginRight = margin + 'px';
+            currentControls.style.marginRight = margin + 'px';
         }
     });
 
     function getControls(event) {
-        var todo = event.target.parentNode;
-        var controls = todo.getElementsByClassName('controls')[0];
+        var parent = event.target.parentNode;
+        while(!parent.classList.contains('todo')) {
+            parent = parent.parentNode;
+        }
+        
+        var controls = parent.getElementsByClassName('controls')[0];
         return controls;
+    }
+
+    function resetControls() {
+        if (!currentControls) return;
+        currentControls.classList.add('transition');
+        var marginText = currentControls.style.marginRight;
+        var margin = +marginText.substr(0, marginText.length - 2);
+        
+        if (margin > -currentControls.offsetWidth / 2 && margin != 0) {
+            currentControls.style.removeProperty('margin-right');
+            currentControls.classList.add('visible');
+        }
+        else if (margin < -currentControls.offsetWidth / 2) {
+            currentControls.style.removeProperty('margin-right');
+            currentControls.classList.remove('visible');
+        }
     }
 })();
